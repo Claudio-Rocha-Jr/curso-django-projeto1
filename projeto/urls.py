f1fe67418f39607d77113b080296b2984de80b3a
Cláudio Rocha Júnior <claudio.rocha-junior@hotmail.com>
2022-06-24 17:29:45 -0300

    Construção do homepage

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
-    path('recipes/', include('recipes.urls')),
 ]
