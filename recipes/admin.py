8fe3ae1eb702f95c38114c28611cb48078f12122
Cláudio Rocha Júnior <claudio.rocha-junior@hotmail.com>
2022-06-28 16:36:19 -0300

    BD

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
+from atexit import register
+
 from django.contrib import admin
 
+from .models import Category, Recipe
+
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
