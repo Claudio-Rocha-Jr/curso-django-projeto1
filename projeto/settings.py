c2e1ca538908d165d3b34c1a2ea1bc17c476e427
Cláudio Rocha Júnior <claudio.rocha-junior@hotmail.com>
2022-06-28 19:51:40 -0300

    Curso

--- projeto/settings.py
+++ projeto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ngah^k)s^nj@&##eyo&&d+(2zz7#6*4upzvl!_2g$+4@cv5xbl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
@@ -7,19 +8,24 @@ from recipes.models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.all().filter(is_published = True).order_by('-id')
     return render(request,'recipes/pages/home.html', context = {
-        'recipes':recipes
+        'recipes':recipes,
+        'page_title':'Home'
     })
 
 def recipe(request, id):
     return render(request,'recipes/pages/recipe-view.html', context = {
-        'recipe':make_recipe(),
+        'recipe':Recipe.objects.filter(id = id).order_by('-id'),
         'is_detail_page':True,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id = category_id).order_by('-id')
+    recipes = Recipe.objects.filter(category__id = category_id,is_published = True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found')
+
     return render(request,'recipes/pages/home.html', context = {
-        'recipes':recipes
-    })
+        'recipes':recipes,
+        'page_title':f'{recipes.first().category.name} - Category'})
