577a710dd6a96b81c83830334fa08ebdbebc9392
Cláudio Rocha Júnior <claudio.rocha-junior@hotmail.com>
2022-06-29 18:53:23 -0300

    Iniciando testes

--- recipes/views.py
+++ recipes/views.py
@@ -15,24 +15,24 @@ def home(request):
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id = category_id,is_published = True).order_by('-id')
+    # recipes = Recipe.objects.filter(category__id = category_id,is_published = True).order_by('-id')
 
-    if not recipes:
-        raise Http404('Not found')
-
-    # recipes = get_list_or_404
-    # (Recipe.objects.filter(
-    #     category__id = category_id,
-    #     is_published = True
-    # ).order_by('-id'))
+    # if not recipes:
+    #     raise Http404('Not found')
+    #category__id is the id column of the related category table
+    recipes = get_list_or_404(Recipe.objects.filter(category__id = category_id,is_published = True).order_by('-id'))
 
     return render(request,'recipes/pages/home.html', context = {
         'recipes':recipes,
-        'page_title':f'{recipes.first().category.name} - Category'})
+        'page_title':f'{recipes[0].category.name} - Category'})
 
 
 def recipe(request, id):
+    #recipe = Recipe.objects.filter(id = id).order_by('-id').first()
+
+    recipe = get_object_or_404(Recipe, id = id, is_published = True)
+
     return render(request,'recipes/pages/recipe-view.html', context = {
-        'recipe':Recipe.objects.filter(id = id).order_by('-id').first(),
+        'recipe':recipe,
         'is_detail_page':True,
     })
