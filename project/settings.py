34b4a4f7ec91da03969e246b3614a218f07e4714
Cláudio Rocha Júnior <claudio.rocha-junior@hotmail.com>
2022-09-14 10:58:33 -0300

    Alterando allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://*.rochaprestadora.com.br','https://*.127.0.0.1']
 
 
 # Application definition
